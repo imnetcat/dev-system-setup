b1992a30d1c19793e8bf7c60d763437d8cf9168a
Guskov Danil <37350375+imnetcat@users.noreply.github.com>
2021-07-07 00:34:37 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#Update
+sudo pacman -Syu --noconfirm
+
+# Nodejs & npm
+sudo pacman -S nodejs npm --noconfirm
+
+# Terminator terminal emulator
+sudo pacman -S terminator --noconfirm
+
+# Utils
+sudo pacman -S curl --noconfirm
