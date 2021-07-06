30b442890e027e7e3a18d43c50d36b33057a0aab
Guskov Danil <37350375+imnetcat@users.noreply.github.com>
2021-07-07 00:56:53 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
-#Update
-sudo pacman -Syu --noconfirm
+#Update && Upgrade
+sudo pacman -Syyu --noconfirm
+
+# Utils
+sudo pacman -S curl git base-devel --noconfirm
 
 # Nodejs & npm
 sudo pacman -S nodejs npm --noconfirm
@@ -7,5 +10,22 @@ sudo pacman -S nodejs npm --noconfirm
 # Terminator terminal emulator
 sudo pacman -S terminator --noconfirm
 
-# Utils
-sudo pacman -S curl --noconfirm
+# Text editor: NeoVim, Visual Studio Code
+sudo pacman -S nvim code
+
+# Docker
+sudo pacman -S docker
+
+# VirtualBox for WM
+sudo pacman -S virtualbox
+
+# AUR package manager: yay
+sudo git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Import configs
+dconf load / < ../appearance/dconf
+mkdir ~/.config/terminator
+cp ../appearance/terminator ~/.config/terminator/config
+cp ../appearance/btcprice.sh ~
