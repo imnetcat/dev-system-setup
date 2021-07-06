b065ba5acf24b64826d45b9431aa6f66dcd0d7a2
Guskov Danil <37350375+imnetcat@users.noreply.github.com>
2021-07-07 01:00:14 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,18 +11,22 @@ sudo pacman -S nodejs npm --noconfirm
 sudo pacman -S terminator --noconfirm
 
 # Text editor: NeoVim, Visual Studio Code
-sudo pacman -S nvim code
+sudo pacman -S nvim code --noconfirm
 
 # Docker
-sudo pacman -S docker
+sudo pacman -S docker --noconfirm
 
 # VirtualBox for WM
-sudo pacman -S virtualbox
+sudo pacman -S virtualbox --noconfirm
 
 # AUR package manager: yay
 sudo git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+
+# Massengers: telegram, slack
+sudo pacman -S telegram-desktop --noconfirm
+yay -S slack-desktop --noconfirm
 
 # Import configs
 dconf load / < ../appearance/dconf
