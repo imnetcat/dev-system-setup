d69834fd1718b11f4c5f2b283c3abf06e25b9047
Guskov Danil <37350375+imnetcat@users.noreply.github.com>
2021-07-08 19:24:19 +0300

    Update appearance.sh

--- appearance.sh
+++ appearance.sh
@@ -1,7 +1,12 @@
 
-# Import configs
+# Import dconf
 dconf load / < ~/dev-system-setup/appearance/dconf
+
+# Import terminator conf
 mkdir ~/.config/terminator
 cp ~/dev-system-setup/appearance/terminator ~/.config/terminator/config
 cp ~/dev-system-setup/appearance/btcprice.sh ~
 chmod +x ~/btcprice.sh
+
+# Import nvim config
+cp ~/dev-system-setup/appearance/init.vim /.config/nvim/init.vim
