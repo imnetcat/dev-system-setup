39d71c37cf0ff55e0f743c273a86f50412be9745
Guskov Danil <37350375+imnetcat@users.noreply.github.com>
2021-07-08 19:20:16 +0300

    Create appearance.sh

---
+++ appearance.sh
@@ -0,0 +1,7 @@
+
+# Import configs
+dconf load / < ~/dev-system-setup/appearance/dconf
+mkdir ~/.config/terminator
+cp ~/dev-system-setup/appearance/terminator ~/.config/terminator/config
+cp ~/dev-system-setup/appearance/btcprice.sh ~
+chmod +x ~/btcprice.sh
